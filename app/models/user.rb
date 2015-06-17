b611c6bf131ef6f9173cf082f7f46a83ad338785
unmanbearpig <unmanbearpig@gmail.com>
2015-06-17 22:46:07 +0300

    Added Devise and a home page

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # TODO: Add confirmable since discourse requires us to confirm emails
+
+  def to_s
+    email
+  end
+end
