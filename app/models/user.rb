2cdf3dfbee0654b50053940d56c8bc5579e3e3ab
unmanbearpig <unmanbearpig@gmail.com>
2015-06-18 18:00:12 +0300

    Added email confirmation
    Required by Discourse, otherwise it would be very insecure.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # TODO: Add confirmable since discourse requires us to confirm emails
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
 
   def to_s
     email
