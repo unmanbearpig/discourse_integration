67fe1df315ca95a8d1fe912725e60a609cf1c697
unmanbearpig <unmanbearpig@gmail.com>
2015-06-18 14:47:26 +0300

    Added link to the forum

--- config/initializers/discourse.rb
+++ config/initializers/discourse.rb
@@ -1,2 +1,3 @@
+DISCOURSE_URL = ENV['DISCOURSE_URL'] || 'http://localhost:8021'
 DISCOURSE_SSO_SECRET = ENV['DISCOURSE_SSO_SECRET'] || 'hellokitty'
 DISCOURSE_SSO_URL = ENV['DISCOURSE_SSO_URL'] || 'http://localhost:8021/session/sso_login'
