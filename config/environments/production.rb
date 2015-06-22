e02730290a5f1c31ad292a0fda129f20580674ab
unmanbearpig <unmanbearpig@gmail.com>
2015-06-22 03:10:09 +0300

    Added hostname config variable for Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: ENV['HOSTNAME'] }
+
 end
 
 ActionMailer::Base.smtp_settings = {
