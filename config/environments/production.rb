31f3ffe4613c9653986bb7e93c3636e0288223c1
unmanbearpig <unmanbearpig@gmail.com>
2015-06-22 03:18:35 +0300

    Configurable email service parameters

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,11 +82,11 @@ Rails.application.configure do
 end
 
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
+  :address        => ENV['MAIL_SERVER'],
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => ENV['MAIL_USERNAME'],
+  :password       => ENV['MAIL_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
